fcb64702ba4e3ec81a868a705377599e891e2c54
Kamalesh Kumaran <139676693+kamalesh1729@users.noreply.github.com>
2024-01-04 10:59:13 +0530

    Add files via upload

---
+++ Eligible vote/Votes.java
@@ -0,0 +1,9 @@
+public class Votes{
+public static void main(String[] args){
+int age=18;
+if(age<18)
+System.out.println("You are not eligible for Vote");
+else
+System.out.println("you are Eligible for Vote");
+}
+}
